01ed4dac2d01b7cda445f54dbf234a767a6ba0c1
Philipp Giese <giese.philipp+git@gmail.com>
2016-10-18 15:41:37 +0200

    removed obsolete files and added more packages

---
+++ client/src/about/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Route } from 'react-router'
+
+import About from './About'
+
+export default (
+  <Route path="about" component={ About } />
+)
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
+
+import 'skeleton-css/css/normalize.css'
+import 'skeleton-css/css/skeleton.css'
+
 import App from './App';
-import './index.css';
+
+import aboutRoutes from './about'
 
 ReactDOM.render(
-  <App />,
+  <Router history={ browserHistory }>
+    <Route path="/" component={ App }>
+      { aboutRoutes }
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
