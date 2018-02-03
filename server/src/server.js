9bc5af8b50cb59895e9e180b476574321e257bcf
Philipp Giese <giese.philipp+git@gmail.com>
2018-02-03 13:48:58 +0100

    logging

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,16 @@ const { PORT = 8080 } = process.env
 
 const app = express()
 
-app.use('/static', express.static(path.join(process.env.PWD, 'server', 'static')))
+console.log(process.env.PWD)
 
-app.get('/*', (req, res) => res.sendFile(path.resolve(process.env.PWD, 'server', 'static', 'index.html')))
+app.use(
+  '/static',
+  express.static(path.join(process.env.PWD, 'server', 'static'))
+)
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.resolve(process.env.PWD, 'server', 'static', 'index.html'))
+)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
