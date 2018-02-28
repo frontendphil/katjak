c95ce5283d034acf4e91b976f07f0410987f0853
Philipp Giese <giese.philipp+git@gmail.com>
2018-02-28 20:45:39 +0100

    added placeholder

--- packages/client/src/about/About.js
+++ packages/client/src/about/About.js
@@ -14,7 +14,15 @@ function About({ style }) {
   return (
     <div id="about">
       <Headline xlarge>About</Headline>
-
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
       <div {...style('container')}>
         <div {...style('section')}>
           <Markdown>{since2016}</Markdown>
@@ -45,6 +53,8 @@ const styled = defaultStyle(({ padding }) => ({
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
+
+    marginTop: padding.large,
   },
 
   section: {
